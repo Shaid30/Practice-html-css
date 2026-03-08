8ff88d4d1f8a8bbf5d74528a22f13671055cf8a2
Abu Shaid <mdabushaid847@gmail.com>
2026-03-08 11:41:26 +0600

    some js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const menu=document.getElementById("menu");
 toggle.addEventListener("click", function(){
     menu.classList.toggle("active");
 });
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click",function(){
+    alert("welcome to shaid's tech world")
+
+});
+
+const loginBtn = document.querySelector(".loginBtn");
+loginBtn.addEventListener("click",function(){
+    alert("welcome to login page")
+});
